56d6a3214bce3fae3293144133a7fdf4c75cdea7
DuhaOdat <odatduha@gmail.com>
2024-09-22 09:17:48 +0300

    MinSchool :)

---
+++ MiniSchool/MiniSchool/Migrations/202409212140129_UpdateUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateUserModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Student", "FullName", c => c.String(nullable: false, maxLength: 300));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Student", "FullName", c => c.String(nullable: false, maxLength: 200));
+        }
+    }
+}
